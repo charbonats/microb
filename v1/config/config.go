d17d802ab767b662a56cb22b5909f6f7e3bdbb09
Guillaume Charbonnier <guillaume.charbonnier@araymond.com>
2024-03-05 19:23:35 +0100

    minimal support for poetry

--- v1/config/config.go
+++ v1/config/config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"net/mail"
 	"os"
 
 	"github.com/BurntSushi/toml"
@@ -34,6 +35,9 @@ func NewConfigFromBytes(data []byte, target string, defaultPythonVersion string)
 		return nil, err
 	}
 	requiresPython := pyproject.Project.RequiresPython
+	if pyproject.Tool.Poetry.Name != "" {
+		requiresPython = pyproject.Tool.Poetry.PythonRequires()
+	}
 	// If no target is specified
 	if target == "" {
 		// Look for the first target in the microb config
@@ -188,7 +192,80 @@ type Author struct {
 // It only contains the microb section and is not a complete representation of the file.
 type Tool struct {
 	Microb Microb `toml:"microb"`
+	Poetry Poetry `toml:"poetry"`
 }
+
+type Poetry struct {
+	Authors      []PoetryAuthor              `toml:"authors"`
+	Name         string                      `toml:"name"`
+	Description  string                      `toml:"description"`
+	Dependencies map[string]PoetryDependency `toml:"dependencies"`
+}
+
+func (p *Poetry) GetAuthors() []Author {
+	var authors []Author
+	for _, a := range p.Authors {
+		authors = append(authors, a.ToAuthor())
+	}
+	return authors
+}
+
+func (p *Poetry) PythonRequires() string {
+	if v, ok := p.Dependencies["python"]; ok {
+		return v.version
+	}
+	return ""
+}
+
+type PoetryAuthor struct {
+	Name  string `toml:"name"`
+	Email string `toml:"email"`
+}
+
+func (p *PoetryAuthor) ToAuthor() Author {
+	return Author{
+		Name:  p.Name,
+		Email: p.Email,
+	}
+}
+
+func (p *PoetryAuthor) UnmarshalTOML(value interface{}) error {
+	text, ok := value.(string)
+	if !ok {
+		return fmt.Errorf("expected string, got %T", value)
+	}
+	addr, err := mail.ParseAddress(text)
+	p.Email = addr.Address
+	p.Name = addr.Name
+	return err
+}
+
+type PoetryDependency struct {
+	version string
+}
+
+func (p *PoetryDependency) UnmarshalTOML(value interface{}) error {
+	text, ok := value.(string)
+	if ok {
+		p.version = text
+		return nil
+	}
+	mapping, ok := value.(map[string]interface{})
+	if !ok {
+		return fmt.Errorf("expected string or map, got %T", value)
+	}
+	if v, ok := mapping["version"]; ok {
+		p.version = v.(string)
+	} else {
+		return fmt.Errorf("version field is required")
+	}
+	return nil
+}
+
+var (
+	_ toml.Unmarshaler = (*PoetryAuthor)(nil)
+	_ toml.Unmarshaler = (*PoetryDependency)(nil)
+)
 
 // Microb is a struct that represents a microb section in a pyproject.toml file.
 // For now, it only contains a map of targets.
